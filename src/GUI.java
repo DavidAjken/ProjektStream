38b9a45a73d79f08b69b056521341317c2c6bc69
DavidAjken <75014951+DavidAjken@users.noreply.github.com>
2020-11-25 12:24:39 +0100

    lavede gui klasse

---
+++ src/GUI.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class GUI {
+    GUI(){
+        System.out.println("hej2");
+    }
+}
--- src/ProjektStream.java
+++ src/ProjektStream.java
@@ -1,5 +1,6 @@
 public class ProjektStream {
     public static void main(String[] args){
         System.out.println("hej");
+        new GUI();
     }
 }
