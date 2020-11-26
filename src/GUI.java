75f21c5cfda0719a71227fdb887413616e243536
DavidAjken <75014951+DavidAjken@users.noreply.github.com>
2020-11-26 14:31:13 +0100

    basic input

--- src/GUI.java
+++ src/GUI.java
@@ -1,11 +1,21 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 import javax.swing.*;
+
+
 public class GUI {
     protected JFrame frame;
 
-    GUI(){
-        makeFrame();
+
+
+
+
+    GUI() throws FileNotFoundException {
+        loadText();
     }
     private void makeFrame()    {
         frame = new JFrame("GUI");
@@ -16,4 +26,27 @@ public class GUI {
 
         frame.setVisible(true);
     }
+
+    public void loadText() throws FileNotFoundException {
+        Scanner sc = new Scanner(getClass().getClassLoader().getResourceAsStream("film.txt"));
+        ArrayList<Film> films = new ArrayList<Film>();
+        sc.useDelimiter("\\s*;\\s*");
+
+        while (sc.hasNext()){
+            String filmName = sc.next();
+            int filmYear = sc.nextInt();
+            String[] filmGenres;
+            String tempGenres = sc.next();
+            filmGenres = tempGenres.split(",");
+
+            double rating = sc.nextDouble();
+            Film film = new Film(filmName,filmGenres,filmYear,rating);
+            films.add(film);
+        }
+        for (Film f: films) {
+            f.print();
+        }
+    }
+
+
 }
