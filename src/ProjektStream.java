bd13ed14ed21f928b50da90c93280bd8d02632d9
DavidAjken <75014951+DavidAjken@users.noreply.github.com>
2020-11-27 10:16:57 +0100

    film textloading virker

--- src/ProjektStream.java
+++ src/ProjektStream.java
@@ -2,7 +2,6 @@ import java.io.FileNotFoundException;
 
 public class ProjektStream {
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("hej");
         new GUI();
     }
 }
