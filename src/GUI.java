4c34dbd579167fc12c98faef98b6a4465a5bf75b
DavidAjken <75014951+DavidAjken@users.noreply.github.com>
2020-12-02 12:11:33 +0100

    Film images
    hver af filmene har nu et tilhørrende billede
    det bilver også tegnet ordenligt

--- src/GUI.java
+++ src/GUI.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Scanner;
 import javax.imageio.ImageIO;
@@ -13,7 +14,11 @@ import javax.swing.*;
 
 
 public class GUI {
+
+    //Dette er den JFrame som bliver vist når programmet kørre
     protected JFrame frame;
+    //Denne ArrayList indeholder alle filmne
+    protected ArrayList<Film> films;
 
     //laver en fil som indeholder alle filmplakaterne
     static final File filmDir = new File("src/filmplakater");
@@ -34,32 +39,29 @@ public class GUI {
 
 
     GUI() throws FileNotFoundException, IOException{
-        makeFrame();
-
+        films = new ArrayList<Film>();
         loadFilmText();
-        //frame.update(frame.getGraphics());
+        loadFilmImages();
+        makeFrame();
     }
 
-    private void makeFrame()    {
+    private void makeFrame() {
 
         frame = new JFrame("GUI");
         frame.setSize(800,800);
 
-        //Container contentPane = frame.getContentPane();
         Container contentPane = frame.getContentPane();
         contentPane.setLayout(new GridLayout(2,1));
 
-        loadFilmImages(contentPane);
-
+        drawFilms(contentPane);
         frame.setVisible(true);
     }
 
     /*
     Denne metode loader alt fra film.txt ind i en arraylist af film
      */
-    public void loadFilmText() throws FileNotFoundException {
+    private void loadFilmText() throws FileNotFoundException {
         Scanner sc = new Scanner(getClass().getClassLoader().getResourceAsStream("film.txt"));
-        ArrayList<Film> films = new ArrayList<Film>();
         sc.useDelimiter("\\s*;\\s*");
 
         while (sc.hasNext()){
@@ -73,30 +75,64 @@ public class GUI {
             Film film = new Film(filmName,filmGenres,filmYear,rating);
             films.add(film);
         }
-        for (Film f: films) {
-            f.print();
-        }
     }
 
-    public void loadFilmImages(Container contentPane) {
-        Container filmScrollPane = new ScrollPane();
-        Container filmBox = new Container();
-        filmBox.setLayout(new GridLayout(0,4,10,10));
-        filmScrollPane.add(filmBox);
-        ImageIcon img = null;
+    /*
+    denne metoder loader filmplakater fra filmplakater mappen
+    og tildeler hver film i ArrayLsiten films en plakat der tilhørre den relevatne film
+     */
+    private void loadFilmImages() throws IOException {
+
+        ImageIcon filmImg = null;
+        HashMap filmImages = new HashMap<String,ImageIcon>();
+
         if(filmDir.isDirectory()){
             for (File f: filmDir.listFiles(IMAGE_FILTER)) {
-                try {
-                    img = new ImageIcon(ImageIO.read(f));
-                    JLabel newFilmImage = new JLabel(img);
-                    filmBox.add(newFilmImage);
-                } catch(IOException e){}
+                filmImg = new ImageIcon(ImageIO.read(f));
+                filmImages.put(f.getName().split(".jpg")[0], filmImg);
             }
         }
 
+        for (Film f: films) {
+            String key = f.getFilmName();
+            filmImg = (ImageIcon) filmImages.get(key);
+            f.setFilmImg(filmImg);
+        }
 
+
+    }
+
+    private void drawFilms(Container contentPane){
+        Container filmScrollPane = new ScrollPane();
+        filmScrollPane.setBackground(new Color(5,5,5));
+        filmScrollPane.doLayout();
+
+        Container filmBox = new Container();
+        filmBox.setLayout(new GridLayout(0,8,10,10));
+
+        ImageIcon filmImg = null;
+        for (Film f: films) {
+            filmImg = f.getFilmImg();
+
+            JLabel newFilm = new JLabel(filmImg);
+            newFilm.setHorizontalAlignment(0);
+
+            //herunder håndteres tegningen af texten
+            newFilm.setText(f.getFilmName());
+            newFilm.setHorizontalTextPosition(0);
+            newFilm.setVerticalTextPosition(3);
+            newFilm.setForeground(new Color(255,255,255));
+
+            //herunder bliver newFilm's størrelse sat
+            newFilm.setMaximumSize(new Dimension(filmImg.getIconWidth(),filmImg.getIconHeight()+20));
+            newFilm.setPreferredSize(new Dimension(filmImg.getIconWidth(),filmImg.getIconHeight()+20));
+            newFilm.setMinimumSize(new Dimension(filmImg.getIconWidth(),filmImg.getIconHeight()+20));
+
+            filmBox.add(newFilm);
+
+        }
+        filmScrollPane.add(filmBox);
         contentPane.add(filmScrollPane);
-
     }
 
 }
