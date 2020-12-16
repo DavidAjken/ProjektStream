8db801361bdf4b4642f78f40e3dad8647db6d369
cwerITU <75118216+cwerITU@users.noreply.github.com>
2020-12-16 10:54:09 +0100

    Bruger updated

--- src/Serier.java
+++ src/Serier.java
@@ -1,32 +1,5 @@
-import javax.swing.*;
 
-public class Serier extends Content {
-    protected String year;
-    protected String[] season;
-
-    public Serier(String serieName, String[] genres, String year, double rating, String[] season) {
-        this.name = serieName;
-        setText(name);
-        this.genres = genres;
-        this.year = year;
-        this.rating = rating;
-        this.season = season;
-        setupLayout();
-    }
-    //get og set metoder til serier, resten er extended til Content
-
-
-    public void popupInfo(JFrame frame) {
-
-        //JOptionPane info = new JOptionPane("hej",);
-        JOptionPane.showMessageDialog(frame, "Serie navn " + name + "\n Genre: " + genreToString() +
-                        "\n Aktive år: " + year + "\n Bedømmese: " + rating,
-                name, JOptionPane.PLAIN_MESSAGE);
-    }
     import javax.swing.*;
-    import javax.swing.border.LineBorder;
-    import java.awt.*;
-    import java.util.ArrayList;
     public class Serier extends Content{
         protected String name;
         protected String[] genres;
@@ -65,13 +38,5 @@ public class Serier extends Content {
                             "\n Aktive år: "+year+"\n Bedømmese: "+rating,
                     name,JOptionPane.PLAIN_MESSAGE);
         }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    public void setSeason(String[] season) {
-        this.season = season;
-    }
 
 }
